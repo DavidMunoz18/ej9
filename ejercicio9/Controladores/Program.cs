087a6c6334169d7d6e53af8109f33c9bc1120643
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2024-03-12 14:11:54 +0100

    Add files via upload

---
+++ ejercicio9/Controladores/Program.cs
@@ -0,0 +1,25 @@
+﻿using ejercicio9.Servicios;
+
+namespace ejercicio9.Controladores
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            OperativaInterfaz oi = new OperativaImplementacion();
+
+            bool cerrarMenu = false;
+
+           
+            do
+            {
+
+                oi.metodoPrincipal();
+
+            } while (!cerrarMenu);
+
+           
+
+        }
+    }
+}
---
+++ ejercicio9/Servicios/OperativaImplementacion.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio9.Servicios
+{
+    internal class OperativaImplementacion : OperativaInterfaz
+    {
+        string frase;
+        public void metodoPrincipal()
+        {
+            solicitarYReemplazar();
+            separarFrase();
+            vocalesFaltan();
+            
+        }
+        char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+        private void solicitarYReemplazar()
+        {
+            Console.WriteLine("Introduzca una frase por consola");
+            frase = Console.ReadLine();
+
+            
+
+            for (int i = 0; i <= vocales.Length - 1; i++)
+            {
+                frase = frase.Replace(vocales[i] , '*');
+            }
+           
+
+            Console.WriteLine(frase);
+
+            
+
+        }
+        private void separarFrase()
+        {
+            string[] fraseSeparada = frase.Split(" ");
+
+
+            DateTime fecha = DateTime.Today;
+
+            string ruta = fecha.Date.ToString("dd-MM-yyyy") + ".txt";
+
+            using(StreamWriter sw = new StreamWriter(ruta)) 
+            {
+                for (int i = 0; i <= fraseSeparada.Length - 1; i++)
+                {
+                    sw.WriteLine(fraseSeparada[i]);
+                }
+
+            }
+
+            String[] leer = File.ReadAllLines(ruta);
+
+           Console.WriteLine("ULTIMAS 2 POSICIONES");
+                for (int i = fraseSeparada.Length - 2 ; i < fraseSeparada.Length; i++)
+                {
+                
+                Console.WriteLine(leer[i]);
+                }
+            
+
+
+           
+        }
+        private void vocalesFaltan()
+        {
+            Console.WriteLine("¿Cuantas vocales faltan?");
+            int numVocales = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int vocale = 0;
+
+            for (int i = 0;i < frase.Length; i++)
+            {
+                if (frase[i] == '*'){
+
+                    vocale++;
+                }
+            }
+            Console.WriteLine("FALTAN: " + vocale);
+
+            char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+
+            if(numVocales == vocale)
+            {
+                
+                for(int i = 0; i < vocale; i++)
+                {
+                    Console.WriteLine("Puedes introducir " + vocale + " " + "vocales");
+                    Console.WriteLine("Introduzca las vocales que faltan");
+                    char vocalesFaltan = Convert.ToChar(Console.ReadLine());                  
+
+                    
+
+                    frase = frase.Replace('*', vocalesFaltan);
+
+                    
+
+                    
+                    
+
+                    
+                    
+                }
+                
+
+                Console.WriteLine(frase);
+
+            }
+          
+
+        }
+       
+       
+    }
+}
